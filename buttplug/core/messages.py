7d18bceaa8e99687fbb744e12a1463640b838264
Kyle Machulis <kyle@nonpolynomial.com>
2019-04-14 00:33:04 -0700

    chore: Move messages to be 3.7 dataclasses, add json decoding

--- buttplug/core/messages.py
+++ buttplug/core/messages.py
@@ -1,127 +1,186 @@
-from json import JSONEncoder
+# TODO Maybe use marshmallow?
 
-class ButtplugMessageEncoder(JSONEncoder):
+from dataclasses import dataclass
+import json
+import sys
+
+
+class ButtplugMessageEncoder(json.JSONEncoder):
     def pascal_case(self, cc_string):
         return ''.join(x.title() for x in cc_string.split('_'))
 
     def build_obj_dict(self, obj):
         # Build camel case versions of our internal variables
-        return dict((self.pascal_case(key), value) for (key, value) in obj.__dict__.items())
+        return dict((self.pascal_case(key), value)
+                    for (key, value) in obj.__dict__.items())
 
     def default(self, obj):
-        return { type(obj).__name__ : self.build_obj_dict(obj) }
+        return {type(obj).__name__: self.build_obj_dict(obj)}
 
-class ButtplugMessage(object):
+
+@dataclass
+class ButtplugMessage:
     SYSTEM_ID = 0
     DEFAULT_ID = 1
 
-    def __init__(self, id = DEFAULT_ID):
-        self.id = id
+    id: int
 
     def as_json(self):
         return ButtplugMessageEncoder().encode(self)
 
+    @staticmethod
+    def from_json(json_str: str):
+        obj = json.loads(json_str)
+        classname = list(obj.keys())[0]
+        cls = getattr(sys.modules[__name__], classname)
+        return cls.from_json(list(obj.values())[0])
+
+
+@dataclass
 class ButtplugDeviceMessage(ButtplugMessage):
-    def __init__(self, id, device_id):
-        super(self, id)
-        self.device_id = device_id
+    device_id: int
+
 
 class ButtplugOutgoingOnlyMessage(object):
     pass
 
+
 class ButtplugDeviceInfoMessage(object):
     pass
 
+
+@dataclass
 class Ok(ButtplugOutgoingOnlyMessage, ButtplugMessage):
-    def __init__(self, id = ButtplugMessage.DEFAULT_ID):
-        super(Ok, self).__init__(id)
+    @staticmethod
+    def from_json(obj: object):
+        return Ok(obj['Id'])
 
+
+@dataclass
 class Error(ButtplugOutgoingOnlyMessage, ButtplugMessage):
-    def __init__(self, error_message, error_code, id = ButtplugMessage.DEFAULT_ID):
-        super(Ok, self).__init__(id)
-        self.error_message = error_message
-        self.error_code = error_code
+    error_message: str
+    error_code: int
 
+    @staticmethod
+    def from_json(obj: object):
+        return Error(obj['Id'], obj['ErrorMessage'], obj['ErrorCode'])
+
+
+@dataclass
 class Test(ButtplugMessage):
-    def __init__(self, test_string, id = ButtplugMessage.DEFAULT_ID):
-        super(Ok, self).__init__(id)
-        self.test_string = test_string
+    test_string: str
 
+    @staticmethod
+    def from_json(obj: object):
+        return Test(obj['Id'], obj['TestString'])
+
+
+@dataclass
 class RequestServerInfo(ButtplugMessage):
-    def __init__(self, client_name, message_version = 1, id = ButtplugMessage.DEFAULT_ID):
-        super(Ok, self).__init__(id)
-        self.client_name = client_name
-        self.message_version = message_version
+    client_name: str
+    message_version: int
 
+    @staticmethod
+    def from_json(obj: object):
+        return RequestServerInfo(obj['Id'], obj['ClientName'], obj['MessageVersion'])
+
+
+@dataclass
 class ServerInfo(ButtplugMessage):
-    def __init__(self, server_name, major_version, minor_version,
-                 build_version, message_version = 1, max_ping_time = 0,
-                 id = ButtplugMessage.DEFAULT_ID):
-        super(Ok, self).__init__(id)
-        self.server_name = server_name
-        self.major_version = major_version
-        self.minor_version = minor_version
-        self.build_version = build_version
-        self.message_version = message_version
-        self.max_ping_time = max_ping_time
+    server_name: str
+    major_version: int
+    minor_version: int
+    build_version: int
+    message_version: int = 1
+    max_ping_time: int = 0
+
+    @staticmethod
+    def from_json(obj: object):
+        return ServerInfo(obj['Id'], obj['ServerName'], obj['MajorVersion'],
+                          obj['MinorVersion'], obj['BuildVersion'],
+                          obj['MessageVersion'], obj['MaxPingTime'])
+
 
 class DeviceList(ButtplugMessage, ButtplugOutgoingOnlyMessage):
     pass
 
+
 class DeviceAdded(ButtplugDeviceInfoMessage, ButtplugOutgoingOnlyMessage):
     pass
+
 
 class DeviceRemoved(ButtplugDeviceInfoMessage, ButtplugOutgoingOnlyMessage):
     pass
 
+
+@dataclass
 class RequestDeviceList(ButtplugMessage):
     pass
 
+
+@dataclass
 class StartScanning(ButtplugMessage):
     pass
 
+
+@dataclass
 class StopScanning(ButtplugMessage):
     pass
 
+
+@dataclass
 class ScanningFinished(ButtplugMessage, ButtplugOutgoingOnlyMessage):
     pass
+
 
 class RequestLog(ButtplugMessage):
     pass
 
+
 class Log(ButtplugMessage, ButtplugOutgoingOnlyMessage):
     pass
 
+
+@dataclass
 class Ping(ButtplugMessage):
     pass
+
 
 class FleshlightLaunchFW12Cmd(ButtplugMessage):
     pass
 
+
 class LovenseCmd(ButtplugMessage):
     pass
+
 
 class KiirooCmd(ButtplugMessage):
     pass
 
+
 class VorzeA10CycloneCmd(ButtplugMessage):
     pass
+
 
 class SingleMotorVibrateCmd(ButtplugMessage):
     pass
 
+
 class VibrateCmd(ButtplugMessage):
     pass
+
 
 class RotateCmd(ButtplugMessage):
     pass
 
+
 class LinearCmd(ButtplugMessage):
     pass
+
 
 class StopDeviceCmd(ButtplugMessage):
     pass
 
+
 class StopAllDevices(ButtplugMessage):
     pass
-
--- tests/test_messages.py
+++ tests/test_messages.py
@@ -1,7 +1,11 @@
 import unittest
-from buttplug.core import messages
+from buttplug.core.messages import ButtplugMessage, Ok
+
 
 class TestMessages(unittest.TestCase):
     def test_message_serialization(self):
-        ok = messages.Ok()
+        ok = Ok(ButtplugMessage.DEFAULT_ID)
         assert ok.as_json() == "{\"Ok\": {\"Id\": 1}}"
+
+    def test_message_deserialization(self):
+        assert ButtplugMessage.from_json("{\"Ok\": {\"Id\": 1}}") == Ok(ButtplugMessage.DEFAULT_ID)
