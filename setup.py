565394cc176fea280b2bf67eacb827098f880b9e
Kyle Machulis <kyle@nonpolynomial.com>
2019-04-13 21:23:51 -0700

    build: Current requirements and setup.py for Py 3.7 Buttplug

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(name="buttplug",
+      author="Nonpolynomial Labs, LLC",
+      author_email="kyle@nonpolynomial.com",
+      description="Python implementation of the Buttplug Intimate Hardware Control Protocol.",
+      long_description="Python implementation of the Buttplug Intimate Hardware Control Protocol.",
+      long_description_content_type="text/markdown",
+      url="https://github.com/buttplugio/buttplug-py",
+      classifiers=[
+          "Programming Language :: Python :: 3",
+          "Development Status :: 3 - Alpha",
+          "License :: OSI Approved :: BSD License",
+          "Operating System :: OS Independent",
+      ],
+      packages=find_packages())
